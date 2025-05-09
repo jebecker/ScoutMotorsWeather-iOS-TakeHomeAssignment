10be2ab09de1a53ed29401be1d5ff3aaaf82c420
Jayme Becker <jaymebecker@icloud.com>
2025-05-09 15:18:15 -0500

    Initial Commit

---
+++ ScoutMotorsWeather/ScoutMotorsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutMotorsWeatherApp.swift
+//  ScoutMotorsWeather
+//
+//  Created by Jayme Becker on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutMotorsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoutMotorsWeatherTests/ScoutMotorsWeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoutMotorsWeatherTests.swift
+//  ScoutMotorsWeatherTests
+//
+//  Created by Jayme Becker on 5/9/25.
+//
+
+import Testing
+
+struct ScoutMotorsWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
